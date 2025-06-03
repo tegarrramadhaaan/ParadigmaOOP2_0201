1d6e5ef2d8a76649e6da42e17c2a9f414674a60c
tegarrramadhaaan <anakrama2000@gmail.com>
2025-06-03 10:20:16 +0700

    Demonstrasi Polimorfisme dengan Fungsi Virtual di C++

---
+++ multiwaris.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
+class pekerja : virtual public orang {
+    public:
+}
