66a436f672cfeb9f7f1ef26696f0d48738573916
tegarrramadhaaan <anakrama2000@gmail.com>
2025-06-03 10:49:51 +0700

    Contoh Enkapsulasi dan Pengaksesan Array Objek dalam C++

--- enkapulasi.cpp
+++ enkapulasi.cpp
@@ -23,5 +23,8 @@ int main() {
 
 	cout << lampuRumah.getSaklarNo(0) << endl;
 	cout << lampuRumah.getSaklarNo(1) << endl;
-	Contoh Enkapsulasi dan Pengaksesan Array Objek dalam C++
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
 }
